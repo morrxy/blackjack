95a1ffd0f566aa623ed25be05d43425cd7df98d8
morrxy <morrxy@gmail.com>
2013-09-03 15:43:54 +0800

    'a'

--- blackjack.rb
+++ blackjack.rb
@@ -25,34 +25,88 @@ include Test::Unit::Assertions
 
 def calculate_total(cards)
   cards_ace = cards.select { |c| c[1] == 'A' }
-  cards_noace = cards.select { |c| c[1] != 'A' }
+  cards_2_to_k = cards.select { |c| c[1] != 'A' }
 
-  val_noace = sum_noace(cards_noace)
+  val_2_to_k = sum_2_to_k(cards_2_to_k)
+  return val_2_to_k if cards_ace.size == 0
 
-  return val_noace if cards_ace.size == 0
+  # all posible uniq sorted total value
+  total_vals = make_total_vals(val_2_to_k, cards_ace.size)
 
-  ace_sums = calculate_ace_sum(cards_ace.size)
+  return 22 if total_vals.first > 21
 
-  sums = []
-  ace_sums.each { |x| sums << x + val_noace }
+  vals_21 = total_vals.select { |v| v == 21 }
+  return 21 if vals_21.size > 0
 
-  arr_21 = sums.select { |x| x == 21 }
-  return 21 if arr_21.size > 0
+  vals_20 = total_vals.select { |v| v < 21 }
+  vals_20.last
+end
 
-  return 22 if sums.first > 21
-
-  arr_20 = sums.select { |x| x < 21 }
-  arr_20.last
+# Array -> Integer
+# produce sum of all cards in the array
+def sum_2_to_k(cards)
+  sum = 0
+  cards.each { |c| sum += calculate_value(c) }
+  sum
 end
 
 # Array -> Integer
 # return value(2-10) for a card(2-K)
 # e.g. ['D', '2'] should return 2, ['D', 'J'] return 10
 def calculate_value(card)
-  # card example: ['C', '2']
   s = card[1]
   return 10 if %w(10 J Q K).include?(s)
   return s.to_i if %w(2 3 4 5 6 7 8 9).include?(s)
+end
+
+# Integer -> Array of Integer
+# produce array of all posible total value for 1 val plus
+# each combination n ace, ace can be 1 or 11
+def make_total_vals(val, n)
+  ace_vals = make_ace_vals(n)
+  total_vals = []
+  ace_vals.each { |x| total_vals << x + val }
+  total_vals
+end
+
+# Integer -> Array
+# asume n >= 1, produce array of uniq combination ace sum for n aces
+# every ace value can be 1 or 11
+def make_ace_vals(n)
+  aces = make_aces(n)
+
+  ace_sets = aces.combination(n).to_a
+
+  ace_vals = calculate_ace_sets(ace_sets)
+
+  ace_vals.uniq!
+  ace_vals.sort!
+end
+
+# Integer -> Array of integer
+# produce array of all n ace value, ace can be 1 or 11
+# e.g. for 1 ace [1, 11] for 2 ace [1, 11, 1, 11]
+def make_aces(n)
+  ace_vals = []
+  n.times do
+    ace_vals << 1
+    ace_vals << 11
+  end
+  ace_vals
+end
+
+# Array of array of Integer -> Array of integer
+# produce an array for each given array's array item's sum
+def calculate_ace_sets(arr)
+  result = []
+
+  arr.each do |item|
+    sum = 0
+    item.each { |i| sum += i }
+    result << sum
+  end
+
+  result
 end
 
 assert_equal(2, calculate_value(%w(S 2)))
@@ -60,47 +114,27 @@ assert_equal(9, calculate_value(%w(S 9)))
 assert_equal(10, calculate_value(%w(S 10)))
 assert_equal(10, calculate_value(%w(S J)))
 
-# Array -> Integer
-# produce sum of all cards in the array
-def sum_noace(cards)
-  sum = 0
-  cards.each { |c| sum += calculate_value(c) }
-  sum
-end
+assert_equal(10, sum_2_to_k([%w(D 10)]))
+assert_equal(14, sum_2_to_k([%w(D 10), %w(S 4)]))
+assert_equal(21, sum_2_to_k([%w(D 2), %w(D 9), %w(S K)]))
 
-assert_equal(10, sum_noace([['D', '10']]))
-assert_equal(14, sum_noace([['D', '10'], ['S', '4']]))
-assert_equal(21, sum_noace([['D', '2'], ['D', '9'], ['S', 'K']]))
+assert_equal([1, 11], make_aces(1))
+assert_equal([1, 11, 1, 11], make_aces(2))
+assert_equal([1, 11, 1, 11, 1, 11], make_aces(3))
+assert_equal([1, 11, 1, 11, 1, 11, 1, 11], make_aces(4))
 
-# Integer -> Array
-# asume n >= 1,produce array of uniq combination ace sum for n aces
-# every ace value can be 1 or 11
-def calculate_ace_sum(n)
-  vals = []
-  n.times do |n|
-    vals << 1
-    vals << 11
-  end
+assert_equal([8, 9], calculate_ace_sets([[8], [9]]))
+assert_equal([5, 16], calculate_ace_sets([[2, 3], [6, 10]]))
+assert_equal([11, 23], calculate_ace_sets([[2, 3, 6], [6, 7, 10]]))
 
-  arr = vals.combination(n).to_a
+assert_equal([1, 11], make_ace_vals(1))
+assert_equal([2, 12, 22], make_ace_vals(2))
+assert_equal([3, 13, 23, 33], make_ace_vals(3))
+assert_equal([4, 14, 24, 34, 44], make_ace_vals(4))
 
-  arr2 = []
-
-  arr.each do |item|
-    sum = 0
-    item.each { |i| sum += i }
-    arr2 << sum
-  end
-
-  arr2.uniq!
-  arr2.sort!
-  arr2
-end
-
-assert_equal([1, 11], calculate_ace_sum(1))
-assert_equal([2, 12, 22], calculate_ace_sum(2))
-assert_equal([3, 13, 23, 33], calculate_ace_sum(3))
-assert_equal([4, 14, 24, 34, 44], calculate_ace_sum(4))
+assert_equal([4, 14], make_total_vals(3, 1))
+assert_equal([6, 16, 26], make_total_vals(4, 2))
+assert_equal([5, 15, 25, 35], make_total_vals(2, 3))
 
 puts 'welcome to blackjack!'
 
@@ -151,7 +185,6 @@ while true
     "for a total of: #{ mytotal }"
     puts 'Blackjack! You win!'
     exit
-    # break
   end
 
   if mytotal > 21
@@ -159,7 +192,6 @@ while true
     "for a total of: #{ mytotal }"
     puts 'Burst! You lose!'
     exit
-    # break
   end
 
   ask_player(dealercards, mycards, dealertotal, mytotal)
